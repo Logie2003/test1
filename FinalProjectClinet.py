b49f8a65d9ad836729a596944715676d750fd7a9
Logie2003 <144826804+Logie2003@users.noreply.github.com>
2023-12-14 14:16:42 -0500

    Add files via upload
    These are the latest and most likely the final changes. Added new GUI

---
+++ FinalProjectClinet.py
@@ -0,0 +1,116 @@
+import socket
+import json
+import PySimpleGUI as sg
+import threading
+import platform
+
+# Check if the platform is Windows
+if platform.system().lower() == 'windows':
+    sg.popup_error('Unsupported platform: Windows', title='Error')
+    exit()
+
+# Unicode symbols for ON and OFF
+CIRCLE = '●'  # Unicode symbol 9899 for ON
+CIRCLE_OUTLINE = '○'  # Unicode symbol 9898 for OFF
+
+def update_gui(json_data, window_main, window_separate): 
+    try:
+        formatted_data = [
+            f"Core Temperature: {json_data['Core Temperature']}",
+            f"GPU Temperature: {json_data['GPU Temperature']}",
+            f"CPU Temperature: {json_data['CPU Temperature']}",
+            f"Voltage: {json_data['Voltage']}",
+            f"GPU Core Speed: {json_data['GPU Core Speed']}",
+            f"HDMI Clock: {json_data['HDMI Clock']}",
+            f"Pixel Values: {json_data['Pixel Values']}",
+            f"H264 block: {json_data['H264 block']}",
+            f"Iteration Count: {json_data['iteration_count']}",
+        ]
+        window_main['-DATA-'].update('\n'.join(formatted_data))
+       
+        # Update LED in the main window
+        window_main['-LED-'].update(CIRCLE if json_data['LED_status'] else CIRCLE_OUTLINE, text_color='green')
+       
+        # Update Iteration Count and Blinking Light in the separate window
+        window_separate['-ITERATION_COUNT-'].update(str(json_data['iteration_count']))
+        window_separate['-LED-'].update(CIRCLE if json_data['LED_status'] else CIRCLE_OUTLINE, text_color='green')
+    except KeyError as e:
+        formatted_data = [f"Error: Key {e} not found in JSON data"]
+        window_main['-DATA-'].update('\n'.join(formatted_data))
+
+def receive_data(client, window_main, window_separate, exit_program):
+    interval_counter = 0
+    while interval_counter < 51 and not exit_program.is_set():
+        try:
+            chunk = client.recv(4096).decode('utf-8') 
+            if not chunk:
+                break
+
+            json_data = json.loads(chunk)
+            update_gui(json_data, window_main, window_separate)
+            interval_counter += 1
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+            break
+
+# Set up the socket client
+client = socket.socket()
+host = '192.168.0.124'  # Use localhost or the actual IP of the Raspberry Pi
+port = 5030
+
+# Define the PySimpleGUI layout for the main window
+layout_main = [
+    [sg.Text("Received Data:", font=('Helvetica', 14))],
+    [sg.Multiline("", key='-DATA-', size=(40, 10), font=('Helvetica', 12), background_color='lightblue')],
+    [sg.Text("", key='-LED-', font=('Helvetica', 14), text_color='green')],
+    [sg.Button('Exit')],
+]
+
+# Create the main PySimpleGUI window
+window_main = sg.Window("Client GUI", layout_main, finalize=True)
+
+# Define the PySimpleGUI layout for the separate GUI
+layout_separate = [
+    [sg.Text("Iteration Count:", font=('Helvetica', 14))],
+    [sg.Text("0", key='-ITERATION_COUNT-', font=('Helvetica', 14))],
+    [sg.Text("", key='-LED-', font=('Helvetica', 14), text_color='green')],
+    [sg.Button('Exit')],
+]
+
+# Create the separate PySimpleGUI window
+window_separate = sg.Window("Iteration Count and Blinking Light", layout_separate, finalize=True)
+
+try:
+    # Connect to the Raspberry Pi's socket server
+    client.connect((host, port))
+
+    # Event to signal the exit of the program
+    exit_program = threading.Event()
+
+    # Start a thread to receive data and update the GUI
+    thread = threading.Thread(target=receive_data, args=(client, window_main, window_separate, exit_program), daemon=True)
+    thread.start()
+
+    # Event loop for the main PySimpleGUI window
+    while True:
+        event_main, values_main = window_main.read(timeout=100)
+        if event_main == sg.WIN_CLOSED or exit_program.is_set() or event_main == 'Exit':
+            break
+
+        event_separate, values_separate = window_separate.read(timeout=100)
+        if event_separate == sg.WIN_CLOSED or exit_program.is_set() or event_separate == 'Exit':
+            break
+
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    # Set the exit event to signal the thread to stop
+    exit_program.set()
+
+    # Close the client socket
+    client.close()
+
+    # Close the PySimpleGUI windows
+    window_main.close()
+    window_separate.close()
---
+++ FinalProjectServer.py
@@ -0,0 +1,67 @@
+import socket
+import json
+import subprocess
+import time
+import threading
+
+def get_sensor_data():
+    try:
+        # Use vcgencmd to get various sensor data
+        core_temp = subprocess.check_output(['vcgencmd', 'measure_temp']).decode('utf-8').strip().replace('temp=', '')
+        gpu_temp = subprocess.check_output(['vcgencmd', 'measure_temp']).decode('utf-8').strip().replace('temp=', '')
+        cpu_temp = subprocess.check_output(['cat', '/sys/class/thermal/thermal_zone0/temp']).decode('utf-8').strip()
+        voltage = subprocess.check_output(['vcgencmd', 'measure_volts']).decode('utf-8').strip()
+        gpu_core_speed = subprocess.check_output(['vcgencmd', 'measure_clock', 'core']).decode('utf-8').strip()
+        hdmi_clock = subprocess.check_output(['vcgencmd', 'measure_clock', 'hdmi']).decode('utf-8').strip()
+        pixel_values = "123, 456, 789"  # Replace with actual pixel values retrieval logic
+        h264_block = subprocess.check_output(['vcgencmd', 'measure_clock', 'h264']).decode('utf-8').strip()
+
+        return {
+            "Core Temperature": core_temp,
+            "GPU Temperature": gpu_temp,
+            "CPU Temperature": f"{float(cpu_temp) / 1000:.2f}",  # Convert to degrees Celsius
+            "Voltage": voltage,
+            "GPU Core Speed": gpu_core_speed,
+            "HDMI Clock": hdmi_clock,
+            "Pixel Values": pixel_values,
+            "H264 block": h264_block,
+        }
+    except Exception as e:
+        print(f"Error getting sensor data: {e}")  
+        return {}
+
+def send_data(client):
+    iteration_count = 0
+    while True:
+        try:
+            sensor_data = get_sensor_data() 
+            if not sensor_data:
+                continue
+
+            sensor_data['iteration_count'] = iteration_count
+            sensor_data['LED_status'] = True if iteration_count % 2 == 0 else False
+
+            json_data = json.dumps(sensor_data)
+            client.sendall(json_data.encode('utf-8'))
+
+            iteration_count += 1 
+            time.sleep(2)  # Adjust the sleep time as needed
+        except Exception as e:
+            print(f"Error sending data: {e}") 
+
+# Set up the socket server
+server = socket.socket()
+host = '192.168.0.124'  # Use the actual IP of the Raspberry Pi
+port = 5030
+
+server.bind((host, port))
+server.listen()
+
+print(f"Server listening on {host}:{port}")
+
+while True:
+    client, addr = server.accept()
+    print(f"Accepted connection from {addr}")
+
+    # Start a thread to send data to the client
+    threading.Thread(target=send_data, args=(client,), daemon=True).start()
